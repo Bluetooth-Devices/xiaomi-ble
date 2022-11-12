0f387c244644a8469de935ce4e5800d882868d81
Jc2k <john.carr@unrouted.co.uk>
2022-11-12 18:34:42 +0000

    feat: add support for HHCCJCY10 (#21)
    * feat: add support for HHCCJCY10
    
    * feat: add new device_classes
    
    Co-authored-by: Ernst Klamer <e.klamer@gmail.com>

--- src/xiaomi_ble/const.py
+++ src/xiaomi_ble/const.py
@@ -1,5 +1,8 @@
 TIMEOUT_1DAY = 86400
 
+SERVICE_MIBEACON = "0000fe95-0000-1000-8000-00805f9b34fb"
+SERVICE_HHCCJCY10 = "0000fd50-0000-1000-8000-00805f9b34fb"
+
 # This characteristic contains the current battery level for a HHCCJCY01
 # as well as the firmware version
 CHARACTERISTIC_BATTERY = "00001a02-0000-1000-8000-00805f9b34fb"
