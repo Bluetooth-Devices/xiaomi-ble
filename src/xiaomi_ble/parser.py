Bluetooth-Devices/xiaomi-ble

commit 1854f71b474508a81c2b50cc0c45de543ddc8aca

fix: adapt presence sensor check to upstream spec change (#256)

src/xiaomi_ble/parser.py
@@ -1121,10 +1121,11 @@ def obj4818(
 def obj484e(
     xobj: bytes, device: XiaomiBluetoothDeviceData, device_type: str
 ) -> dict[str, Any]:
-    """From miot-spec: occupancy-status: uint8: 0 - No One, 1 - Has One"""
-    """Translate to: occupancy: bool: 0 - Clear, 1 - Detected"""
+    """From miot-spec: occupancy-status: uint8: 0 - No One, 1 - Has One,
+    2 - Quick Check"""
+    """Translate to: occupancy: bool: 0 - Clear, 1 - Detected, 2 - Clear"""
     device.update_predefined_binary_sensor(
-        BinarySensorDeviceClass.OCCUPANCY, xobj[0] > 0
+        BinarySensorDeviceClass.OCCUPANCY, xobj[0] == 1
     )
     return {}
 

tests/test_parser.py
@@ -3746,7 +3746,7 @@ def test_Xiaomi_ES3_occupancy_on():
         },
         binary_entity_values={
             KEY_BINARY_OCCUPANCY: BinarySensorValue(
-                device_key=KEY_BINARY_OCCUPANCY, name="Occupancy", native_value=True
+                device_key=KEY_BINARY_OCCUPANCY, name="Occupancy", native_value=False
             ),
         },
     )

commit 34ec4ab5256d86b64417ab6c519ae3e12a7a9a58

fix: fix regression in occupancy status check
Fix the regression from commit
1854f71b474508a81c2b50cc0c45de543ddc8aca: only apply the third status
value for the given occupancy sensor model.
It might apply for other Xiaomi occupancy sensors but to be safe only
apply it to the model where it has been tested.

src/xiaomi_ble/parser.py
@@ -1121,12 +1121,18 @@ def obj4818(
 def obj484e(
     xobj: bytes, device: XiaomiBluetoothDeviceData, device_type: str
 ) -> dict[str, Any]:
-    """From miot-spec: occupancy-status: uint8: 0 - No One, 1 - Has One,
-    2 - Quick Check"""
-    """Translate to: occupancy: bool: 0 - Clear, 1 - Detected, 2 - Clear"""
-    device.update_predefined_binary_sensor(
-        BinarySensorDeviceClass.OCCUPANCY, xobj[0] == 1
-    )
+    """From miot-spec: occupancy-status: uint8: 0 - No One, 1 - Has One"""
+    """Translate to: occupancy: bool: 0 - Clear, 1 - Detected"""
+    if device_type == "XMOSB01XS":
+        """Translate to: occupancy: bool: 0 - Clear, 1 - Detected, 2 - Clear"""
+        device.update_predefined_binary_sensor(
+            BinarySensorDeviceClass.OCCUPANCY, xobj[0] == 1
+        )
+    else:
+        """Translate to: occupancy: bool: 0 - Clear, 1 - Detected"""
+        device.update_predefined_binary_sensor(
+            BinarySensorDeviceClass.OCCUPANCY, xobj[0] > 0
+        )
     return {}
 
 

tests/test_parser.py
@@ -3746,7 +3746,7 @@ def test_Xiaomi_ES3_occupancy_on():
         },
         binary_entity_values={
             KEY_BINARY_OCCUPANCY: BinarySensorValue(
-                device_key=KEY_BINARY_OCCUPANCY, name="Occupancy", native_value=False
+                device_key=KEY_BINARY_OCCUPANCY, name="Occupancy", native_value=True
             ),
         },
     )